e09c4578e590afdca53b6059bb3ce928894a37b3
terra2o <terra2o@protonmail.com>
2026-08-05 23:41:31 +0300

    feat: command line arguments in main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,18 +3,29 @@
 #include <ftxui/dom/elements.hpp>
 
 #include <filesystem>
+#include <iostream>
 
 #include "core/directoryReader.hpp"
 #include "core/fileEntry.hpp"
 #include "core/fileEntryFormat.hpp"
 #include "ui/fileListComponent.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc >= 3)
+    {
+        std::cerr << "There must be one argument.";
+        return 1;
+    }
+
     using namespace ftxui;
 
     auto fileList = Make<dosyaci::FileListComponent>(MenuOption::VerticalAnimated());
-    fileList->setFile(std::filesystem::current_path());
+
+    if (argc == 1)
+        fileList->setFile(std::filesystem::current_path());
+    else if (argc == 2)
+        fileList->setFile(argv[1]);
 
     auto screen = ScreenInteractive::Fullscreen();
     screen.Loop(fileList);
